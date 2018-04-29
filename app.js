ee0dad5d19a8f3e324788dcd6306041af44aa5be
matthewstewart <matthew.stewart.mi@gmail.com>
2018-04-29 16:05:48 -0700

    add response

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.get('/api/v0/monitor', function(req, res) {
 app.post('/routing-table', (req, res) => {
   console.log('Route Worked');
   console.log(req);
+  res.json({"message": "Example Response"});
   // req.setEncoding('utf8');
   // res.locals.rawBody = '';
   // req.on(data, (chunk) => {
