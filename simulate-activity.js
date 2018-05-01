bd1bf32e0bb2801e5ddb00bdc17231c149f13170
jhpoelen <jhpoelen@xs4all.nl>
2018-04-30 17:48:54 -0700

    update example

--- simulate-activity.js
+++ simulate-activity.js
@@ -5,16 +5,18 @@ env(__dirname + '/.env');
 
 var exitnodeIp = '45.34.140.42';
 var options = {
-  url: `http://localhost:${process.env.PORT}?numberOfGateways=15&numberOfRoutes=21`,
+  url: `http://localhost:${process.env.PORT}/api/v0/monitor`,
   // the server authenticates the request by looking at
   // its source ip or x-forwarded-for header
   headers: {
-    'x-forwarded-for': exitnodeIp
-  }
+    'x-forwarded-for': exitnodeIp,
+    'content-type': 'application/json'
+  },
+  body: '{ "numberOfGateways": 15, "numberOfRoutes": 21 }'
 };
 
-console.log(`GET ${options.url}`);
-request(options, (error, response) => {
+console.log(`POST ${options.url}`);
+request.post(options, (error, response) => {
   if (!error && response.statusCode == 200) {
     console.log('success');
   } else {
