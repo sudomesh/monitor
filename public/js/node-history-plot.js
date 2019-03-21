546b93d62ccb05f1e80fea5681e993bb1510f37f
Benny Lichtner <bennlich@gmail.com>
2019-03-20 22:01:16 -0700

    add internet-archive to monitor; change plot colors a bit

--- public/js/node-history-plot.js
+++ public/js/node-history-plot.js
@@ -62,9 +62,10 @@
     // Associate exitnode IPs with specific colors. It's confusing if every time you load
     // the UI, a different color is used for each exitnode.
     let colors = {
-      '64.71.176.94': 'steelblue',
+      '64.71.176.94': '#F2813F',
       '45.34.140.42': '#b85bdc',
-      '107.170.219.5': '#3eb2f3'
+      '107.170.219.5': '#3eb2f3',
+      '208.70.31.83': '#7FDB5A'
     };
     // Use backup colors for exitnodes we don't know about ahead of time.
     let otherColors = ['#adadad', 'black'];
--- src/exitnodeIPs.js
+++ src/exitnodeIPs.js
@@ -1,7 +1,8 @@
 // IP addresses in this list are granted permission to post status updates to the monitor server
 
 module.exports = [
+  '208.70.31.83',
+  '107.170.219.5',
   '45.34.140.42',
-  '64.71.176.94',
-  '107.170.219.5'
+  '64.71.176.94'
 ];
