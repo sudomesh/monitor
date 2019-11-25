8056529c21bfb96b2b6ca771cca4f98246183469
Benny Lichtner <bennlich@gmail.com>
2019-11-24 22:00:45 -0800

    Update monitor.sh
    Use our own subdomain so we're not tied to heroku

--- monitor.sh
+++ monitor.sh
@@ -9,6 +9,6 @@
 ROUTES=$(ip route | grep via | grep -v default | awk '{print $1 "\t" $3  }' | sort | uniq | wc -l)
 GATEWAYS=$(ip route | grep via | grep -v default | awk '{print $3 }' | sort | uniq | wc -l)
 
-URL="https://peoplesopen.herokuapp.com/api/v0/monitor"
+URL="https://monitor.peoplesopen.net/api/v0/monitor"
 
 curl -H "Content-Type: application/json" -X POST -d "{ \"numberOfRoutes\": $ROUTES, \"numberOfGateways\": $GATEWAYS }" $URL > /dev/null
