a60ffed7ed8da6d40422c15d67c3646a0276005c
Benny Lichtner <bennlich@gmail.com>
2019-11-24 22:01:22 -0800

    Update post-routing-table.sh
    use our own subdomain so we're not tied to heroku

--- post-routing-table.sh
+++ post-routing-table.sh
@@ -4,4 +4,4 @@
 # their routing table to the monitor server
 
 DATA=$(ip route | grep via | grep -v default | awk '{print $1 "," $3  }' | sort | uniq | tr '\n' '|')
-curl --silent -H 'Content-Type: text/plain' -d $DATA -X POST https://peoplesopen.herokuapp.com/api/v0/nodes > /dev/null 
+curl --silent -H 'Content-Type: text/plain' -d $DATA -X POST https://monitor.peoplesopen.net/api/v0/nodes > /dev/null 
